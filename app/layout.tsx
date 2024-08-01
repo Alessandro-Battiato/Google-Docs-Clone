b4fc56e4703736c2ed400f82aae514d9ddd94193
alessandrobattiato <alessandroDev@outlook.it>
2024-08-01 20:52:59 +0200

    feat: test initial render

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,19 @@
-import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
 
+import "./globals.css";
+
 import { cn } from "@/lib/utils";
+import { Metadata } from "next";
 
 const fontSans = FontSans({
     subsets: ["latin"],
     variable: "--font-sans",
 });
+
+export const metadata: Metadata = {
+    title: "LiveDocs",
+    description: "Your go-to collaborative editor",
+};
 
 export default function RootLayout({
     children,
@@ -15,14 +22,13 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <head />
             <body
                 className={cn(
-                    "min-h-screen bg-background font-sans antialiased",
+                    "min-h-screen font-sans antialiased",
                     fontSans.variable
                 )}
             >
-                ...
+                {children}
             </body>
         </html>
     );
