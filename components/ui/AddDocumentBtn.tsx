8e726d5581118c60ceddfe6900e9cc440612b432
alessandrobattiato <alessandroDev@outlook.it>
2024-09-08 16:39:47 +0200

    feat: add create room handler function

--- components/ui/AddDocumentBtn.tsx
+++ components/ui/AddDocumentBtn.tsx
@@ -2,9 +2,23 @@
 import React from "react";
 import { Button } from "./button";
 import Image from "next/image";
+import { createDocument } from "@/lib/actions/room.actions";
+import { useRouter } from "next/navigation";
 
 const AddDocumentBtn = ({ userId, email }: AddDocumentBtnProps) => {
-    const addDocumentHandler = async () => {};
+    const router = useRouter();
+
+    const addDocumentHandler = async () => {
+        try {
+            const room = await createDocument({ userId, email });
+
+            if (room) {
+                router.push(`/documents/${room.id}`);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
     return (
         <Button
